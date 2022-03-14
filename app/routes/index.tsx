24c71aaf471f8e731fc03127476376b78ddb93ad
Vic Vijayakumar <needcaffeine@gmail.com>
2022-03-14 09:34:08 -0400

    feat: get data from supabase and render on the site

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,32 @@
-export default function Index() {
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+import type { Beverage } from "@prisma/client";
+
+// Import the Prisma client
+import db from "~/lib/db.server";
+
+// Each route can define a "loader" function that will be called
+// on the server before rendering to provide data to the route.
+type LoaderData = { items: Array<Beverage> };
+export const loader: LoaderFunction = async () => {
+  const data = {
+    items: await db.beverage.findMany(),
+  };
+
+  return data;
+};
+
+export default function IndexRoute() {
+  // This hook returns the JSON parsed data from your route loader function.
+  const data = useLoaderData<LoaderData>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      {data.items.map((item) => (
+        <span key={item.id} style={{ marginRight: "2px" }}>
+          {item.name === "coffee" ? <span>🟫</span> : <span>🟦</span>}
+        </span>
+      ))}
+    </>
   );
 }
